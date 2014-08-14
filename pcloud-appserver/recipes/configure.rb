6648988042392e462810678c307330443bfc3fa5
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-14 13:45:35 +0800

    Update recipes
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- pcloud-appserver/recipes/configure.rb
+++ pcloud-appserver/recipes/configure.rb
@@ -11,12 +11,6 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  #cookbook_file "/srv/www/personal_cloud_portal/current/config/mailer.yml" do
-    #source "mailer.yml"
-    #mode 0644
-    #action :create_if_missing
-  #end
-
   execute "restart Rails app #{application}" do
     cwd deploy[:current_path]
     command node[:opsworks][:rails_stack][:restart_command]
@@ -37,19 +31,5 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  #template "#{deploy[:deploy_to]}/shared/config/database.yml" do
-    #source "database.yml.erb"
-    #cookbook 'rails'
-    #mode "0660"
-    #group deploy[:group]
-    #owner deploy[:user]
-    #variables(:database => deploy[:database], :environment => deploy[:rails_env])
-
-    #notifies :run, "execute[restart Rails app #{application}]"
-
-    #only_if do
-      #deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    #end
-  #end
 end
 
