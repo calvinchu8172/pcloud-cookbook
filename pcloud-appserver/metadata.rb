83255146dca7b7b2e7a0f2462d104839ff3e8075
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-18 16:48:47 +0800

    Refactoring
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- pcloud-appserver/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'pcloud-appserver'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures pcloud-appserver'
-long_description 'Installs/Configures pcloud-appserver'
-version          '0.1.0'
-
