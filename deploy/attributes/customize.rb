b57f648afb529729e17585e5cb9d2fb40cd12d0f
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-20 17:03:09 +0800

    Add template of config/settings/production.yml
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,3 +1,4 @@
 normal[:opsworks][:rails][:ignore_bundler_groups] = []
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/mailer.yml'] = 'config/mailer.yml'
+normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/settings.production.yml'] = 'config/settings/production.yml'
 #default[:deploy][application][:symlink_before_migrate] = {}
