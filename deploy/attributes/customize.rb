d17200b4b3a6f549746f34313aaac5706c4f2105
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-08-21 10:15:48 +0800

    Add template of config/secrets.yml
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,4 +1,5 @@
+# project specific settings (which override defaults)
 normal[:opsworks][:rails][:ignore_bundler_groups] = []
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/mailer.yml'] = 'config/mailer.yml'
+normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/secrets.yml'] = 'config/secrets.yml'
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/settings.production.yml'] = 'config/settings/production.yml'
-#default[:deploy][application][:symlink_before_migrate] = {}
--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -33,10 +33,24 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     owner deploy[:user]
 
-    #notifies :run, "execute[restart Rails app #{application}]"
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source "secrets.yml.erb"
+    cookbook 'portalapp'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables({
+      :production_secret_key => 'c894834ec3f545ba9f1495e4a68be7db6c25ba3400439349c2e86684845c90fd7a6eccf666cc651c2f1b38a80585c8cf4ed7466a15acdba2b9701402a5af2aef'
+    })
 
     only_if do
       File.directory?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
+
 end
