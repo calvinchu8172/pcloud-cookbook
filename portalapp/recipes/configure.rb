f42d3ad58d1ee3566f6a778fcc570bdf9163ce07
Travis Liu <travisliu@ecoworkinc.com>
2014-08-27 12:20:21 +0800

    remove portal config restart server command

--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -3,11 +3,11 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  execute "restart Rails app #{application}" do
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
+  #execute "restart Rails app #{application}" do
+  #  cwd deploy[:current_path]
+  #  command node[:opsworks][:rails_stack][:restart_command]
+  #  action :nothing
+  #end
 
   node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
   deploy = node[:deploy][application]
