d962a98a5623b7dbcacfb8d063abe62f6780aa3b
Travis Liu <travisliu@ecoworkinc.com>
2014-08-28 14:14:43 +0800

    remove deploy user

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,7 +1,7 @@
 # project specific settings (which override defaults)
 #normal[:opsworks][:rails][:ignore_bundler_groups] = []
 normal[:rails][:max_pool_size] = 20
-normal[:deploy][:user] = 'deploy'
+#normal[:deploy][:user] = 'deploy'
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/mailer.yml'] = 'config/mailer.yml'
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/secrets.yml'] = 'config/secrets.yml'
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/settings.production.yml'] = 'config/settings/production.yml'
