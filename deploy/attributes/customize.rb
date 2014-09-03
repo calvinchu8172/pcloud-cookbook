7b5f96c15df130844b2136152935bb1ad5a0a377
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-03 15:20:46 +0800

    Update attributes
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -6,7 +6,7 @@ normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/secret
 normal[:deploy]['personal_cloud_portal'][:symlink_before_migrate]['config/settings.production.yml'] = 'config/settings/production.yml'
 
 ## Personal Cloud Bots
-normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['key_id'] = ''
-normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['secret_key'] = ''
-normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['bucket'] = ''
-normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['log_path'] = ''
+normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['key_id'] = 'AKIAIFBA53YARL2VHDWQ'
+normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['secret_key'] = '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'
+normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['bucket'] = 'ecowork-demo'
+normal[:deploy]['personal_cloud_bots']['fluentd']['s3']['log_path'] = 'personal-cloud-bot-log/'
