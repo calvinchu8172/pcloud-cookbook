3cf4fe4d541344ddf3d6623e7c053cfaf01bf2f2
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-03 11:08:30 +0800

    Improve bots deploy recipe

--- deploy/definitions/opsworks_deploy_bots.rb
+++ deploy/definitions/opsworks_deploy_bots.rb
@@ -1,12 +1,4 @@
 define :opsworks_deploy_bots do
   application = params[:app]
   deploy = params[:deploy_data]
-
-  directory "#{deploy[:deploy_to]}" do
-    group deploy[:group]
-    owner deploy[:user]
-    mode "0775"
-    action :create
-    recursive true
-  end
 end
--- deploy/recipes/bots.rb
+++ deploy/recipes/bots.rb
@@ -4,19 +4,27 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("#{application.inspect}")
   Chef::Log.info("#{deploy.inspect}")
 
-  # NOTICE: Remember to set your App's name to 'Personal Cloud Bots'
-  if deploy[:application] == 'personal_cloud_bots'
-    opsworks_deploy_dir do
-      user deploy[:user]
-      group deploy[:group]
-      path deploy[:deploy_to]
-    end
+  # NOTICE: Remember to set your App's name exactly to 'Personal Cloud Bots'
+  if deploy[:application] != 'personal_cloud_bots'
+    # skip it
+    next
+  end
 
-    opsworks_deploy_bots do
-      # send application & deploy object as parameters to 
-      # ../definitions/opsworks_deploy_bots.rb
-      deploy_data deploy
-      app application
-    end
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy_bots do
+    # send application & deploy object as parameters to 
+    # ../definitions/opsworks_deploy_bots.rb
+    deploy_data deploy
+    app application
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
   end
 end
