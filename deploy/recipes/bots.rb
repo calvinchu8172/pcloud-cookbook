ce7a10eb9b536008c7500e984ed5e4295455630a
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-03 10:38:00 +0800

    Improve bots deploy recipe
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/recipes/bots.rb
+++ deploy/recipes/bots.rb
@@ -3,4 +3,17 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   Chef::Log.info("#{application.inspect}")
   Chef::Log.info("#{deploy.inspect}")
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy_bots do
+    # send application & deploy object as parameters to 
+    # ../definitions/opsworks_deploy_bots.rb
+    deploy_data deploy
+    app application
+  end
 end
