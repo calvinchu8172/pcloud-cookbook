1c21f674e4e4db430c34a7c6ac944b4948c679ab
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-23 16:30:50 +0800

    Update REST API Server deploy recipe
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/recipes/rest_api_server.rb
+++ deploy/recipes/rest_api_server.rb
@@ -1,26 +1,20 @@
 include_recipe 'deploy'
 
-node[:deploy].each do |application, deploy|
+application = 'personal_cloud_rest_api'
+deploy = node[:deploy][application]
 
-  if deploy[:application_type] != 'rails' && deploy[:application] != 'personal_cloud_rest_api'
-    Chef::Log.debug("Skipping deploy::rails application #{application} as it is neither a Rails app nor Personal Cloud REST API Server")
-    next
-  end
-
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
-  opsworks_rails do
-    deploy_data deploy
-    app application
-  end
-
-  opsworks_deploy_rest_api_server do
-    deploy_data deploy
-    app application
-  end
+opsworks_deploy_dir do
+  user deploy[:user]
+  group deploy[:group]
+  path deploy[:deploy_to]
 end
 
+opsworks_rails do
+  deploy_data deploy
+  app application
+end
+
+opsworks_deploy_rest_api_server do
+  deploy_data deploy
+  app application
+end
