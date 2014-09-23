360f41530abdc51fc07db37165089cf1b610275e
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-23 11:45:51 +0800

    Add Portal deploy recipe
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ deploy/recipes/portal.rb
@@ -0,0 +1,26 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+
+  if deploy[:application_type] != 'rails' && deploy[:application] != 'personal_cloud_portal'
+    Chef::Log.debug("Skipping deploy::rails application #{application} as it is neither a Rails app nor Personal Cloud Portal")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_rails do
+    deploy_data deploy
+    app application
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
+
