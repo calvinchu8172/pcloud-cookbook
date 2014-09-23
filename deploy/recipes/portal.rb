1047e6bf6a2a7b3928e97939d59323f835377e13
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-23 16:26:01 +0800

    Update Portal deploy recipe
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/recipes/portal.rb
+++ deploy/recipes/portal.rb
@@ -1,26 +1,20 @@
 include_recipe 'deploy'
 
-node[:deploy].each do |application, deploy|
+application = 'personal_cloud_portal'
+deploy = node[:deploy][application]
 
-  if deploy[:application_type] != 'rails' && deploy[:application] != 'personal_cloud_portal'
-    Chef::Log.debug("Skipping deploy::rails application #{application} as it is neither a Rails app nor Personal Cloud Portal")
-    next
-  end
-
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
-  opsworks_rails do
-    deploy_data deploy
-    app application
-  end
-
-  opsworks_deploy_portalapp do
-    deploy_data deploy
-    app application
-  end
+opsworks_deploy_dir do
+  user deploy[:user]
+  group deploy[:group]
+  path deploy[:deploy_to]
 end
 
+opsworks_rails do
+  deploy_data deploy
+  app application
+end
+
+opsworks_deploy_portalapp do
+  deploy_data deploy
+  app application
+end
