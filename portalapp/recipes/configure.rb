fa0ec66b19e439aeed2ce1cade95c6084645c29e
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-24 11:41:49 +0800

    Revert "Strip duplicated codes"
    This reverts commit 246a5d4f5e03a7f6e65784947ca6692b18c724ad.

--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -10,6 +10,42 @@ execute "restart Rails app #{application}" do
   action :nothing
 end
 
+node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+deploy = node[:deploy][application]
+
+template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+  source "database.yml.erb"
+  cookbook 'rails'
+  mode "0660"
+  group deploy[:group]
+  owner deploy[:user]
+  variables(:database => deploy[:database], :environment => deploy[:rails_env])
+
+  notifies :run, "execute[restart Rails app #{application}]"
+
+  only_if do
+    deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+  end
+end
+
+template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
+  source "memcached.yml.erb"
+  cookbook 'rails'
+  mode "0660"
+  group deploy[:group]
+  owner deploy[:user]
+  variables(
+    :memcached => deploy[:memcached] || {},
+    :environment => deploy[:rails_env]
+  )
+
+  notifies :run, "execute[restart Rails app #{application}]"
+
+  only_if do
+    deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+  end
+end
+
 template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
   source "mailer.yml.erb"
   cookbook 'portalapp'
