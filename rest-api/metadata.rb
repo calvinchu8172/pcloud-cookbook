0a4c57c4112d8ece1a011d149030491bad863a64
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-24 10:34:13 +0800

    Add rest-api cookbook
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ rest-api/metadata.rb
@@ -0,0 +1,8 @@
+name             'rest-api'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rest-api'
+long_description 'Installs/Configures rest-api'
+version          '0.1.0'
+
