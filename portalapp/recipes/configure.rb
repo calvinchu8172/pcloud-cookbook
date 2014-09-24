267be755c4b9faf08eb8323b33217ed6dc87963a
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-24 11:43:16 +0800

    Revert "Update Portal App configure recipe"
    This reverts commit bb933ce3dcbb16eb4fa37b1141703f88e01824ad.

--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -1,92 +1,61 @@
 include_recipe "deploy"
 
-application = 'personal_cloud_portal'
-deploy = node[:deploy][application]
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
 
-execute "restart Rails app #{application}" do
-  user 'deploy'
-  cwd deploy[:current_path]
-  command node[:opsworks][:rails_stack][:restart_command]
-  action :nothing
-end
-
-node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
-deploy = node[:deploy][application]
-
-template "#{deploy[:deploy_to]}/shared/config/database.yml" do
-  source "database.yml.erb"
-  cookbook 'rails'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
-  variables(:database => deploy[:database], :environment => deploy[:rails_env])
-
-  notifies :run, "execute[restart Rails app #{application}]"
-
-  only_if do
-    deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+  execute "restart Rails app #{application}" do
+    user 'deploy'
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
   end
-end
 
-template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-  source "memcached.yml.erb"
-  cookbook 'rails'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
-  variables(
-    :memcached => deploy[:memcached] || {},
-    :environment => deploy[:rails_env]
-  )
+  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+  deploy = node[:deploy][application]
 
-  notifies :run, "execute[restart Rails app #{application}]"
+  template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
+    source "mailer.yml.erb"
+    cookbook 'portalapp'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
 
-  only_if do
-    deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
   end
-end
 
-template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
-  source "mailer.yml.erb"
-  cookbook 'portalapp'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
+  template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
+    source "production.yml.erb"
+    cookbook 'portalapp'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
 
-  notifies :run, "execute[restart Rails app #{application}]"
+    notifies :run, "execute[restart Rails app #{application}]"
 
-  only_if do
-    File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
   end
-end
 
-template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
-  source "production.yml.erb"
-  cookbook 'portalapp'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source "secrets.yml.erb"
+    cookbook 'portalapp'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables({
+      :production_secret_key => 'c894834ec3f545ba9f1495e4a68be7db6c25ba3400439349c2e86684845c90fd7a6eccf666cc651c2f1b38a80585c8cf4ed7466a15acdba2b9701402a5af2aef'
+    })
 
-  notifies :run, "execute[restart Rails app #{application}]"
+    notifies :run, "execute[restart Rails app #{application}]"
 
-  only_if do
-    File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
   end
-end
 
-template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
-  source "secrets.yml.erb"
-  cookbook 'portalapp'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
-  variables({
-    :production_secret_key => 'c894834ec3f545ba9f1495e4a68be7db6c25ba3400439349c2e86684845c90fd7a6eccf666cc651c2f1b38a80585c8cf4ed7466a15acdba2b9701402a5af2aef'
-  })
-
-  notifies :run, "execute[restart Rails app #{application}]"
-
-  only_if do
-    File.directory?("#{deploy[:deploy_to]}/shared/config/")
-  end
 end
