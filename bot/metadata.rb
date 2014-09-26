e318f73ba6337c189ef31a17071d490d174f0f26
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-26 11:57:08 +0800

    Remove unused bot cookbook
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- bot/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'bot'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures bot'
-long_description 'Installs/Configures bot'
-version          '0.1.0'
-
