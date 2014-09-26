feb750760aa2904118696a688613bd42e00055d0
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-09-26 18:34:40 +0800

    Add MongooseIM cookbook
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ mongooseim/metadata.rb
@@ -0,0 +1,8 @@
+name             'mongooseim'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mongooseim'
+long_description 'Installs/Configures mongooseim'
+version          '0.1.0'
+
