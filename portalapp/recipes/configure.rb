fca570110c020ce6c478276868965b5c297bbd2e
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2014-09-27 14:46:42 +0800

    Use custom JSON values for mailer settings
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -2,6 +2,7 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
+  portalapp_settings = node['pcloud_settings']['portalapp']
 
   execute "restart Rails app #{application}" do
     user 'deploy'
@@ -13,12 +14,20 @@ node[:deploy].each do |application, deploy|
   node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
   deploy = node[:deploy][application]
 
+  mailer_settings = portalapp_settings['mailer']['production']
+
   template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
     source "mailer.yml.erb"
     cookbook 'portalapp'
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
+    variables({
+      :delivery_method => mailer_settings['delivery_method'],
+      :smtp_settings => mailer_settings['smtp_settings'],
+      :default => mailer_settings['default'],
+      :default_url_options => mailer_settings['default_url_options']
+    })
 
     notifies :run, "execute[restart Rails app #{application}]"
 
