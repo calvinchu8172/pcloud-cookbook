febbc15526d67adcde71b827c2eea9d9d71f95a0
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2014-09-27 19:22:27 +0800

    Use custom JSON values for mailer & production settings
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- portalapp/recipes/configure.rb
+++ portalapp/recipes/configure.rb
@@ -1,59 +1,55 @@
 include_recipe "deploy"
 
-node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
-  portalapp_settings = node['pcloud_settings']['portalapp']
+application = 'personal_cloud_portal'
+deploy = node[:deploy][application]
+portalapp_settings = node['pcloud_settings']['portalapp']
 
-  execute "restart Rails app #{application}" do
-    user 'deploy'
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
+execute "restart Rails app #{application}" do
+  user 'deploy'
+  cwd deploy[:current_path]
+  command node[:opsworks][:rails_stack][:restart_command]
+  action :nothing
+end
+
+mailer_settings = portalapp_settings['mailer']['production']
+
+template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
+  source "mailer.yml.erb"
+  cookbook 'portalapp'
+  mode "0660"
+  group deploy[:group]
+  owner deploy[:user]
+  variables({
+    :delivery_method => mailer_settings['delivery_method'],
+    :smtp_settings => mailer_settings['smtp_settings'],
+    :default => mailer_settings['default'],
+    :default_url_options => mailer_settings['default_url_options']
+  })
+
+  notifies :run, "execute[restart Rails app #{application}]"
+
+  only_if do
+    File.directory?("#{deploy[:deploy_to]}/shared/config/")
   end
+end
 
-  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
-  deploy = node[:deploy][application]
+production_settings = portalapp_settings['production']
 
-  mailer_settings = portalapp_settings['mailer']['production']
+template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
+  source "production.yml.erb"
+  cookbook 'portalapp'
+  mode "0660"
+  group deploy[:group]
+  owner deploy[:user]
+  variables({
+    :magic_number => production_settings['magic_number'],
+    :xmpp => production_settings['xmpp'],
+    :environments => production_settings['environments'],
+  })
 
-  template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
-    source "mailer.yml.erb"
-    cookbook 'portalapp'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables({
-      :delivery_method => mailer_settings['delivery_method'],
-      :smtp_settings => mailer_settings['smtp_settings'],
-      :default => mailer_settings['default'],
-      :default_url_options => mailer_settings['default_url_options']
-    })
+  notifies :run, "execute[restart Rails app #{application}]"
 
-    notifies :run, "execute[restart Rails app #{application}]"
-
-    only_if do
-      File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    end
-  end
-
-  production_settings = portalapp_settings['production']
-
-  template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
-    source "production.yml.erb"
-    cookbook 'portalapp'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables({
-      :magic_number => production_settings['magic_number'],
-      :xmpp => production_settings['xmpp'],
-      :environments => production_settings['environments'],
-    })
-
-    notifies :run, "execute[restart Rails app #{application}]"
-
-    only_if do
-      File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    end
+  only_if do
+    File.directory?("#{deploy[:deploy_to]}/shared/config/")
   end
 end
--- rest-api/recipes/configure.rb
+++ rest-api/recipes/configure.rb
@@ -2,6 +2,7 @@ include_recipe "deploy"
 
 application = 'personal_cloud_rest_api'
 deploy = node[:deploy][application]
+rest_api_server_settings = node['pcloud_settings']['rest-api-server']
 
 execute "restart Rails app #{application}" do
   user 'deploy'
@@ -10,41 +11,7 @@ execute "restart Rails app #{application}" do
   action :nothing
 end
 
-node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
-deploy = node[:deploy][application]
-
-template "#{deploy[:deploy_to]}/shared/config/database.yml" do
-  source "database.yml.erb"
-  cookbook 'rails'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
-  variables(:database => deploy[:database], :environment => deploy[:rails_env])
-
-  notifies :run, "execute[restart Rails app #{application}]"
-
-  only_if do
-    deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-  end
-end
-
-template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-  source "memcached.yml.erb"
-  cookbook 'rails'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
-  variables(
-    :memcached => deploy[:memcached] || {},
-    :environment => deploy[:rails_env]
-  )
-
-  notifies :run, "execute[restart Rails app #{application}]"
-
-  only_if do
-    deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-  end
-end
+mailer_settings = rest_api_server_settings['mailer']['production']
 
 template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
   source "mailer.yml.erb"
@@ -52,6 +19,12 @@ template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
   mode "0660"
   group deploy[:group]
   owner deploy[:user]
+  variables({
+    :delivery_method => mailer_settings['delivery_method'],
+    :smtp_settings => mailer_settings['smtp_settings'],
+    :default => mailer_settings['default'],
+    :default_url_options => mailer_settings['default_url_options']
+  })
 
   notifies :run, "execute[restart Rails app #{application}]"
 
@@ -59,6 +32,8 @@ template "#{deploy[:deploy_to]}/shared/config/mailer.yml" do
     File.directory?("#{deploy[:deploy_to]}/shared/config/")
   end
 end
+
+production_settings = rest_api_server_settings['production']
 
 template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
   source "production.yml.erb"
@@ -66,22 +41,10 @@ template "#{deploy[:deploy_to]}/shared/config/settings.production.yml" do
   mode "0660"
   group deploy[:group]
   owner deploy[:user]
-
-  notifies :run, "execute[restart Rails app #{application}]"
-
-  only_if do
-    File.directory?("#{deploy[:deploy_to]}/shared/config/")
-  end
-end
-
-template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
-  source "secrets.yml.erb"
-  cookbook 'portalapp'
-  mode "0660"
-  group deploy[:group]
-  owner deploy[:user]
   variables({
-    :production_secret_key => 'c894834ec3f545ba9f1495e4a68be7db6c25ba3400439349c2e86684845c90fd7a6eccf666cc651c2f1b38a80585c8cf4ed7466a15acdba2b9701402a5af2aef'
+    :magic_number => production_settings['magic_number'],
+    :xmpp => production_settings['xmpp'],
+    :environments => production_settings['environments'],
   })
 
   notifies :run, "execute[restart Rails app #{application}]"
