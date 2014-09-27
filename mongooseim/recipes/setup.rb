8f79919dcb00c8121e213b69d1466aab7a2b73f2
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2014-09-27 20:17:42 +0800

    Install MongooseIM
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- mongooseim/recipes/setup.rb
+++ mongooseim/recipes/setup.rb
@@ -1,5 +1,12 @@
 execute "install mongooseim" do
   user "root"
   cwd "/tmp"
-  command "wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb"
+  command <<-EOH
+    wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb \
+    dpkg -i erlang-solutions_1.0_all.deb \
+    apt-get update \
+    apt-get -y install mongooseim \
+    apt-get -y install mysql-client-5.6 \
+    apt-get -y install redis-tools
+  EOH
 end
