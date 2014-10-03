cb8d1f89a051177eba655468dfbdf75f95fb4d24
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-10-03 15:28:30 +0800

    Update Bots deploy recipe
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- deploy/definitions/opsworks_deploy_bots.rb
+++ deploy/definitions/opsworks_deploy_bots.rb
@@ -3,6 +3,18 @@ define :opsworks_deploy_bots do
   deploy = params[:deploy_data]
   bots_settings = node['pcloud_settings']['bots']
 
+  bots_instances_data_s3 = bots_settings['instances']['s3']
+
+  execute "load bot instances data from S3" do
+    user "root"
+    cwd "#{deploy[:deploy_to]}/shared/config/"
+    command <<-EOH
+      AWS_ACCESS_KEY_ID=#{bots_instances_data_s3['key_id']} \
+      AWS_SECRET_ACCESS_KEY=#{bots_instances_data_s3['secret_key']} \
+      aws s3 cp s3://#{bots_instances_data_s3['bucket']}/bots/bots.json bots.json --region 'us-east-1'
+    EOH
+  end
+
   # Setting-up & Running fluentd
   fluentd_s3 = bots_settings['fluentd']['s3']
 
