ce12f27585254d10a39e739a3c29b704c95bbb59
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-10-04 11:27:22 +0800

    Remove bots cookbook (again)
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- bots/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'bots'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures bots'
-long_description 'Installs/Configures bots'
-version          '0.1.0'
-
