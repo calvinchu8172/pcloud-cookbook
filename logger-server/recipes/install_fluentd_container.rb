9191330574cde54c7ab5d74fda9b57664a181457
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-11-24 20:13:22 +0800

    Build docker image
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- logger-server/recipes/install_fluentd_container.rb
+++ logger-server/recipes/install_fluentd_container.rb
@@ -2,3 +2,8 @@ cookbook_file "Dockerfile" do
   path "/srv/Dockerfile"
   action :create
 end
+
+execute "build docker image" do
+  cwd "/srv"
+  command "docker build -t personal_cloud/fluentd ."
+end
