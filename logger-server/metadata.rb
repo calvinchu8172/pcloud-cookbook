7ba60f12c2e8e99fb0941b92192899d05b28e839
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-11-24 15:53:41 +0800

    Install official Docker
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ logger-server/metadata.rb
@@ -0,0 +1,7 @@
+name             'logger-server'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures logger-server'
+long_description 'Installs/Configures logger-server'
+version          '0.1.0'
---
+++ logger-server/recipes/install_official_docker.rb
@@ -0,0 +1,19 @@
+package "apt-transport-https" do
+  action :install
+end
+
+execute "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9" do
+  user "root"
+end
+
+execute "add official docker repository" do
+  user "root"
+  command <<-EOH
+    echo 'deb https://get.docker.com/ubuntu docker main' > /etc/apt/sources.list.d/docker.list && \
+    apt-get update
+  EOH
+end
+
+package "lxc-docker" do
+  action :install
+end
