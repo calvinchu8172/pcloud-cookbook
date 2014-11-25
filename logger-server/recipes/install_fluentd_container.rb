56eb251b307a195e3f3b791b327a8b4f0ac1f082
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-11-25 11:41:47 +0800

    Run fluentd container
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- logger-server/recipes/install_fluentd_container.rb
+++ logger-server/recipes/install_fluentd_container.rb
@@ -7,3 +7,8 @@ execute "build docker image" do
   cwd "/srv"
   command "docker build -t personal_cloud/fluentd ."
 end
+
+execute "run fluentd container" do
+  cwd "/srv"
+  command "docker run -d personal_cloud/fluentd"
+end
