78d8e958c86e7f2045b4fc1fc6a5b469c1c19965
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-11-25 16:22:30 +0800

    Code refactor & build project specific image
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ logger-server/files/default/out_hipchatv2.rb
@@ -0,0 +1,79 @@
+module Fluent
+  class Hipchatv2Output < BufferedOutput
+    COLORS = %w(yellow red green purple gray random)
+    FORMAT = %w(html text)
+    Fluent::Plugin.register_output('hipchatv2', self)
+
+    config_param :api_token, :string
+    config_param :default_room, :string, :default => nil
+    config_param :default_color, :string, :default => nil
+    config_param :default_from, :string, :default => nil
+    config_param :default_notify, :bool, :default => false
+    config_param :default_format, :string, :default => nil
+    config_param :default_mention, :string, :default => nil
+    config_param :default_timeout, :time, :default => nil
+    config_param :http_proxy_host, :string, :default => nil
+    config_param :http_proxy_port, :integer, :default => nil
+    config_param :http_proxy_user, :string, :default => nil
+    config_param :http_proxy_pass, :string, :default => nil
+    config_param :flush_interval, :time, :default => 1
+
+    attr_reader :hipchat
+
+    def initialize
+      super
+      require 'hipchat'
+    end
+
+    def configure(conf)
+      super
+
+      api_token = conf['api_token']
+      log.debug 'api token: ' + api_token
+      @hipchat = HipChat::Client.new(api_token, :api_version => 'v2')
+      @default_room = conf['default_room']
+      @default_from = conf['default_from'] || 'fluentd'
+      @default_notify = conf['default_notify'] || false
+      @default_mention = conf['default_mention']
+      @default_color = conf['default_color'] || 'yellow'
+      @default_format = conf['default_format'] || 'html'
+      @default_timeout = conf['default_timeout']
+    end
+
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
+
+    def write(chunk)
+      chunk.msgpack_each do |(tag,time,record)|
+        begin
+          send_message(record)
+          set_topic(record) if record['topic']
+        rescue => e
+          $log.error("HipChat Error:", :error_class => e.class, :error => e.message)
+        end
+      end
+    end
+
+    def send_message(record)
+      room = record['room'] || @default_room
+      from = record['from'] || @default_from
+      message = record.to_s
+      message = '@' + @default_mention + ' ' + message if @default_mention
+      notify = @default_notify
+
+      color = COLORS.include?(record['color']) ? record['color'] : @default_color
+      message_format = FORMAT.include?(record['format']) ? record['format'] : @default_format
+
+      log.debug 'send to hipchat room:' + room + ', from:' + from + ', message:' + message + ', format:' + message_format + ', notify:' + notify.to_s + ', color:' + color
+      @hipchat[room].send(from, message, :message_format => message_format, :notify => notify == 'true', :color => color)
+    end
+
+    def set_topic(record)
+      room = record['room'] || @default_room
+      from = record['from'] || @default_from
+      topic = record['topic']
+      @hipchat[room].topic(topic, :from => from)
+    end
+  end
+end
--- logger-server/recipes/install_fluentd_container.rb
+++ logger-server/recipes/install_fluentd_container.rb
@@ -8,20 +8,40 @@ cookbook_file "Dockerfile-ecowork-fluentd" do
   action :create
 end
 
-execute "build docker image" do
+execute "build fluentd docker base image" do
   cwd "/srv/ecowork_fluentd"
   command "docker build -t ecowork/fluentd ."
 end
 
+cookbook_file "Dockerfile-personal-cloud-fluentd" do
+  path "/srv/personal_cloud_fluentd/Dockerfile"
+  action :create
+end
+
+cookbook_file "fluent.conf" do
+  path "/srv/personal_cloud_fluentd/fluent.conf"
+  action :create
+end
+
+cookbook_file "out_hipchatv2.rb" do
+  path "/srv/personal_cloud_fluentd/out_hipchatv2.rb"
+  action :create
+end
+
+execute "build personal cloud specific fluentd docker image" do
+  cwd "/srv/personal_cloud_fluentd"
+  command "docker build -t personal_cloud/fluentd ."
+end
+
 execute "kill fluentd container" do
   command <<-EOH
-    docker ps -a | grep "ecowork/fluentd" | cut -d" " -f1 | xargs sudo docker kill && \
-    docker ps -a | grep "ecowork/fluentd" | cut -d" " -f1 | xargs sudo docker rm
+    docker ps -a | grep "personal_cloud/fluentd" | cut -d" " -f1 | xargs sudo docker kill && \
+    docker ps -a | grep "personal_cloud/fluentd" | cut -d" " -f1 | xargs sudo docker rm
   EOH
 
   only_if "ps ef | grep fluentd | grep ruby | grep -v grep"
 end
 
 execute "run fluentd container" do
-  command "docker run -d ecowork/fluentd"
+  command "docker run -d personal_cloud/fluentd"
 end
