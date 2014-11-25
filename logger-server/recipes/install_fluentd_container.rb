16f93e6cab5836ae9dba1f1cbf52aef160728087
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2014-11-25 11:55:25 +0800

    Refactor docker setup codes
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- logger-server/recipes/install_fluentd_container.rb
+++ logger-server/recipes/install_fluentd_container.rb
@@ -1,14 +1,18 @@
-cookbook_file "Dockerfile" do
-  path "/srv/Dockerfile"
+execute "mkdir for Docker files" do
+  cwd "/srv"
+  command "mkdir ecowork_fluentd personal_cloud_fluentd"
+end
+
+cookbook_file "ecowork_fluentd/Dockerfile" do
+  path "/srv/ecowork_fluentd/Dockerfile"
   action :create
 end
 
 execute "build docker image" do
-  cwd "/srv"
-  command "docker build -t personal_cloud/fluentd ."
+  cwd "/srv/ecowork_fluentd"
+  command "docker build -t ecowork/fluentd ."
 end
 
 execute "run fluentd container" do
-  cwd "/srv"
-  command "docker run -d personal_cloud/fluentd"
+  command "docker run -d ecowork/fluentd"
 end
