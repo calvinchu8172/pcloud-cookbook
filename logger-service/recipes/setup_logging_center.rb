401ce8156192f8159be9a2e86994d70793048871
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-02-25 11:31:40 +0800

    Add fluent-plugin-cloudwatch-logs
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- logger-service/recipes/setup_logging_center.rb
+++ logger-service/recipes/setup_logging_center.rb
@@ -8,12 +8,6 @@ end
 
 # setup fluentd logging center
 
-#cookbook_file "Dockerfile" do
-  #source "fluentd-center/Dockerfile"
-  #path "/srv/fluentd-center/Dockerfile"
-  #action :create
-#end
-
 template "/srv/fluentd-center/Dockerfile" do
   source 'fluentd-center/Dockerfile.erb'
   mode '0644'
@@ -30,17 +24,6 @@ cookbook_file "fluent.conf" do
   path "/srv/fluentd-center/fluent.conf"
   action :create
 end
-
-#template "/srv/fluentd-center/fluent.conf" do
-  #source 'fluentd-center/fluent.conf.erb'
-  #mode '0644'
-  #owner 'root'
-  #group 'root'
-  #variables({
-    #:layer => node[:opsworks][:stack][:name].squeeze.downcase.tr(" ", "_"),
-    #:hostname => node[:opsworks][:instance][:hostname]
-  #})
-#end
 
 cookbook_file "out_hipchatv2.rb" do
   source "fluentd-center/out_hipchatv2.rb"
