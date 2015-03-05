4c09c336c4c27dcf5400c6cfcfcf23c92936d151
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-03-05 13:05:37 +0800

    Add Kibana
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- logger-service/recipes/install_kibana.rb
+++ logger-service/recipes/install_kibana.rb
@@ -1,14 +1,17 @@
+kibana = "kibana-4.0.1-linux-x64"
+
 execute "download kibana" do
   cwd "/tmp"
-  command "wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz"
-  not_if "test -f /tmp/kibana-4.0.1-linux-x64.tar.gz"
+  command "wget https://download.elasticsearch.org/kibana/kibana/#{kibana}.tar.gz"
+  not_if "test -f /tmp/#{kibana}.tar.gz"
 end
 
 execute "setup kibana" do
   cwd "/srv"
-  command <<-EOH
-    tar xvfz /tmp/kibana-4.0.1-linux-x64.tar.gz && \
-    cd kibana-4.0.1-linux-x64 && \
-    ./bin/kibana &
-  EOH
+  command "tar xvfz /tmp/#{kibana}.tar.gz"
+end
+
+execute "run kibana" do
+  cwd "/srv/#{kibana}"
+  command "./bin/kibana &"
 end
