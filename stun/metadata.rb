a9a3da98a6983311291b33d247ea330724b2651a
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-04-27 15:41:23 +0800

    Add STUN cookbook
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ stun/metadata.rb
@@ -0,0 +1,8 @@
+name             'stun'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stun'
+long_description 'Installs/Configures stun'
+version          '0.1.0'
+depends          'common'
