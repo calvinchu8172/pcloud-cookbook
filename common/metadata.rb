57934f8b57cf9229690f7b2341e2430e3ce8e539
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-04-27 14:29:47 +0800

    Add common cookbook
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ common/metadata.rb
@@ -0,0 +1,7 @@
+name             'common'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures common'
+long_description 'Installs/Configures common'
+version          '0.1.0'
