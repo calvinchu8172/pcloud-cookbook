546c747d6e0861af3423c1bca27dc83828077f71
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-04-27 19:01:53 +0800

    Setup STUN server in Docker
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- stun/recipes/setup_stun_server.rb
+++ stun/recipes/setup_stun_server.rb
@@ -1,1 +1,21 @@
 include_recipe "common::install_official_docker" 
+
+execute "mkdir for Docker files" do
+  cwd "/srv"
+  command "mkdir -p stunserver"
+end
+
+cookbook_file "Dockerfile" do
+  source "Dockerfile"
+  path "/srv/stunserver/Dockerfile"
+  action :create
+end
+
+execute "build stunserver docker image" do
+  cwd "/srv/stunserver"
+  command "docker build -t stunserver ."
+end
+
+execute "run stunserver in docker" do
+  command "docker run -d --net=host stunserver /opt/stunserver/stunserver --mode full --primaryinterface eth0 --altinterface eth1"
+end
