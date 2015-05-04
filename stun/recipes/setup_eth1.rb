810b0aa9e58f8e446be7c7b4c4f99aa81d653964
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-05-04 14:56:26 +0800

    Try to install AWS SDK for Ruby
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- stun/recipes/setup_eth1.rb
+++ stun/recipes/setup_eth1.rb
@@ -5,7 +5,8 @@ ENV['AWS_REGION'] = "us-east-1"
 Chef::Log.info("Setup a secondary NIC with an Elastic IP address")
 
 Aws.config.update({
-  region: 'us-east-1'
+  region: 'us-east-1',
+  credentials: Aws::InstanceProfileCredentials.new(), 
 })
 
 client = Aws::EC2::Client.new()
