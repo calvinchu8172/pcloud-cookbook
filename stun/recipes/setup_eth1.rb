36b078f180c52853d3f78cac11ec3fa88e9d9090
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-05-04 14:31:30 +0800

    Try to get network interfaces list
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- stun/recipes/setup_eth1.rb
+++ stun/recipes/setup_eth1.rb
@@ -1,3 +1,6 @@
 ENV['AWS_REGION'] = "us-east-1"
 
 Chef::Log.info("Setup a secondary NIC with an Elastic IP address")
+
+resource = Aws::EC2::Resource.new
+Chef::Log.info(resource.network_interfaces)
