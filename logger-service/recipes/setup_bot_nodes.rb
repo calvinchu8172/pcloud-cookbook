26eeb3c5f82abbf64da125292e7c13fa344009da
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-05-11 16:58:08 +0800

    Add Dockerized Fluentd for bot nodes
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

---
+++ logger-service/recipes/setup_bot_nodes.rb
@@ -0,0 +1,26 @@
+include_recipe "common::install_official_docker" 
+include_recipe "logger-service::install_fluentd_container"
+
+execute "mkdir for Docker files" do
+  cwd "/srv"
+  command "mkdir -p fluentd-bot-nodes"
+end
+
+cookbook_file "Dockerfile" do
+  source "fluentd-bot-nodes/Dockerfile"
+  path "/srv/fluentd-bot-nodes/Dockerfile"
+  action :create
+end
+
+template "/srv/fluentd-portal-nodes/fluent.conf" do
+  source 'fluentd-portal-nodes/fluent.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables({
+    :layer => node[:opsworks][:stack][:name].squeeze.downcase.tr(" ", "_"),
+    :hostname => node[:opsworks][:instance][:hostname],
+    :fluentd1 => node['loggers']['fluentd1'],
+    :fluentd2 => node['loggers']['fluentd2']
+  })
+end
