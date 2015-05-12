b9c7b6b8dba624aa4a38ef857ff43bfc35b2b437
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-05-12 14:56:11 +0800

    Fix if fake XMPP servers were not listed in custom JSON
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- opsworks_stack_state_sync/recipes/hosts.rb
+++ opsworks_stack_state_sync/recipes/hosts.rb
@@ -1,6 +1,12 @@
 require 'resolv'
 
-fake_xmpp_server = node['pcloud_settings']['bots']['fake_xmpp_server'].nil? ? nil : node['pcloud_settings']['bots']['fake_xmpp_server']
+fake_xmpp_server = nil
+
+unless node['pcloud_settings'].nil?
+  unless node['pcloud_settings']['bots'].nil?
+    fake_xmpp_server = node['pcloud_settings']['bots']['fake_xmpp_server'].nil? ? nil : node['pcloud_settings']['bots']['fake_xmpp_server']
+  end
+end
 
 template '/etc/hosts' do
   source "hosts.erb"
