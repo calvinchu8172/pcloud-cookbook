2ab2f8ca8e4a079492393aac41bf63f7189047b1
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-07-30 10:52:50 +0800

    execute whenever
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- setup/recipes/ddns_expire_cron.rb
+++ setup/recipes/ddns_expire_cron.rb
@@ -1,8 +1,11 @@
 if (node[:opsworks][:instance][:hostname] == "rails-app1" && node[:opsworks][:instance][:layers].include?("rails-app"))
-  Chef::Log.info("Assign DDNS expiration cron job on #{node[:opsworks][:instance][:hostname]}")
-  Chef::Log.info("#{node[:deploy]}")
+  rails_env = node[:deploy]['personal_cloud_portal'][:rails_env]
+  app_path = node[:deploy]['personal_cloud_portal']['current_path']
 
-  #execute "run whenever" do
-    #command "bundle exec whenever --update-crontab --set 'environment=staging'"
-  #end
+  Chef::Log.info("Assign DDNS expiration cron job on #{node[:opsworks][:instance][:hostname]} (cwd: #{app_path})")
+
+  execute "run whenever" do
+    cwd app_path
+    command "RAILS_ENV=#{rails_env} bundle exec whenever --update-crontab --set 'environment=#{rails_env}'"
+  end
 end
