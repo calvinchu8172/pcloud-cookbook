f2f473d9cfa88f617c69df59245282153c915cf2
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-07-30 10:40:51 +0800

    Get node[:deploy] attributes
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- setup/recipes/ddns_expire_cron.rb
+++ setup/recipes/ddns_expire_cron.rb
@@ -1,1 +1,8 @@
-Chef::Log.info("#{node[:opsworks][:instance]}")
+if (node[:opsworks][:instance][:hostname] == "rails-app1" && node[:opsworks][:instance][:layers].include?("rails-app"))
+  Chef::Log.info("Assign DDNS expiration cron job on #{node[:opsworks][:instance][:hostname]}")
+  Chef::Log.info("#{node[:deploy]}")
+
+  #execute "run whenever" do
+    #command "bundle exec whenever --update-crontab --set 'environment=staging'"
+  #end
+end
