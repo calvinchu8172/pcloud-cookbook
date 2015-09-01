51c17dfc6c3fee9573b281d366ea899b6c9228bd
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-09-01 12:50:31 +0800

    Use Chef cron resource to setup a cronjob instead
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- setup/recipes/ddns_expire_cron.rb
+++ setup/recipes/ddns_expire_cron.rb
@@ -5,7 +5,7 @@ if (node[:opsworks][:instance][:hostname] == "rails-app1" && node[:opsworks][:in
   Chef::Log.info("Assign DDNS expiration cron job on #{node[:opsworks][:instance][:hostname]} (cwd: #{app_path})")
 
   cron "ddns expiration" do
-    time :hourly
+    minute '0,30'
     user 'deploy'
     path "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games" 
     command "/bin/bash -l -c 'cd /srv/www/personal_cloud_portal/current && RAILS_ENV=staging bundle exec rake ddns_expire:cronjob --silent >> log/cron.log 2>&1'"
