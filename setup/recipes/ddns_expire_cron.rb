83eb2ab824b86da9d9b6eb7984330162d420fca0
Calvin Chu <calvinchu@ecoworkinc.com>
2015-11-02 12:48:31 +0800

    Feature 功能 #17204
    * 修改執行ddns expire cron的時間，改成每天執行一次，時間為：美東時間2:00，等於台灣時間10:00
    
    ref #17204

--- setup/recipes/ddns_expire_cron.rb
+++ setup/recipes/ddns_expire_cron.rb
@@ -5,9 +5,10 @@ if (node[:opsworks][:instance][:hostname] == "rails-app1" && node[:opsworks][:in
   Chef::Log.info("Assign DDNS expiration cron job on #{node[:opsworks][:instance][:hostname]} (cwd: #{app_path})")
 
   cron "ddns expiration" do
-    minute '0,30'
+    minute '0'
+    hour '2'
     user 'deploy'
-    path "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games" 
+    path "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
     command "/bin/bash -l -c 'cd #{app_path} && RAILS_ENV=#{rails_env} bundle exec rake ddns_expire:cronjob --silent >> /dev/null 2>&1'"
   end
 end
