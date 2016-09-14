4ae374a8b89cbda548fd34bcdf67214ba83277fc
Calvin Chu <calvinchu@ecoworkinc.com>
2016-09-14 15:02:58 +0800

    Remove ssoapp2(deploy in Personal Cloud RESTSPI Server) cookbook.

--- ssoapp2/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'portalapp'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures portalapp'
-long_description 'Installs/Configures portalapp'
-version          '0.1.0'
-
