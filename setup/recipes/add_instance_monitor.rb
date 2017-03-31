2b1e2386cd835f04283bc8f6c1d89418020be15c
Ryan Li <ryanli@ecoworkinc.com>
2017-03-31 13:19:41 +0800

    debug

--- setup/recipes/add_instance_monitor.rb
+++ setup/recipes/add_instance_monitor.rb
@@ -5,6 +5,7 @@ instance_setup_alarm_settings = node['ec2_instance_settings']['setup_alarm']
 stack    = node[:opsworks][:stack][:name]
 hostname = node[:opsworks][:instance][:hostname]
 aws_instance_id = node["opsworks"]["instance"]["aws_instance_id"]
+account = instance_setup_alarm_settings['sns_resource'].split(':')[4]
 
 directory "/opt/bin" do
   owner 'root'
@@ -23,7 +24,8 @@ template "/opt/bin/instance_monitor_alarm.sh" do
     :region => instance_setup_alarm_settings['region'],
     :stack => stack,
     :hostname => hostname,
-    :aws_instance_id => aws_instance_id
+    :aws_instance_id => aws_instance_id,
+    :account => account
   })
 end
 
